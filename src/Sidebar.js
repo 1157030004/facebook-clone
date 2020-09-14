d7241b0144c468af7cf8e4318d719b0191f8c5a8
Ariq Dhia Irfanudin <invanteris@gmail.com>
2020-09-14 17:36:36 +0700

    header, siderow, feed and widgets are added

---
+++ src/Sidebar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Sidebar.css";
+import SidebarRow from "./SidebarRow";
+import LocalHospitalIcon from "@material-ui/icons/LocalHospital";
+import EmojiFlagsIcon from "@material-ui/icons/EmojiFlags";
+import PeopleIcon from "@material-ui/icons/People";
+import ChatIcon from "@material-ui/icons/Chat";
+import StorefrontIcon from "@material-ui/icons/Storefront";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+import { ExpandMoreOutlined } from "@material-ui/icons";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <SidebarRow
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTPHfjDx0hI8iNQJHWhCXnb7ruJeVwpfh-kmvgETKideQ&usqp=CAU&ec=45699843"
+        title="Shadee"
+      />
+      <SidebarRow
+        Icon={LocalHospitalIcon}
+        title="COVID-19 Information Center"
+      />
+      <SidebarRow Icon={EmojiFlagsIcon} title="Pages" />
+      <SidebarRow Icon={PeopleIcon} title="Friends" />
+      <SidebarRow Icon={ChatIcon} title="Messenger" />
+      <SidebarRow Icon={StorefrontIcon} title="Marketplace" />
+      <SidebarRow Icon={VideoLibraryIcon} title="Videos" />
+      <SidebarRow Icon={ExpandMoreOutlined} title="See More" />
+    </div>
+  );
+}
+
+export default Sidebar;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBMH5aQkMTcYN89cyvxS1hNZYukDCmzE8E",
+  authDomain: "facebook-clone-89be6.firebaseapp.com",
+  databaseURL: "https://facebook-clone-89be6.firebaseio.com",
+  projectId: "facebook-clone-89be6",
+  storageBucket: "facebook-clone-89be6.appspot.com",
+  messagingSenderId: "938325800159",
+  appId: "1:938325800159:web:2e402a4eca0af17d12256a",
+  measurementId: "G-LGV12ZQYGF",
+};
